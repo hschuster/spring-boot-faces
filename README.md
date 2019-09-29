4c370f25d8eeb20d8e5cc5d9667f5e4d45b3a8f5
helmut <hschuster.xo@gmail.com>
2019-09-29 21:46:39 +0200

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+## JavaServer Faces with Spring Boot
+
+https://dzone.com/articles/developing-jsf-applications-with-spring-boot
+https://github.com/auth0-blog/spring-boot-faces
---
+++ src/main/java/com/auth0/samples/bootfaces/Product.java
@@ -0,0 +1,24 @@
+package com.auth0.samples.bootfaces;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column
+	private String name;
+	@Column
+	private BigDecimal price;
+	protected Product() {
+	}
+	public Product(String name, BigDecimal price) {
+		this.name = name;
+		this.price = price;
+	}
+}
