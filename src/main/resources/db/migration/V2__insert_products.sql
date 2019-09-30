352ac9370798a9e9ce629ddd0cdf1fb64dfc5018
Schuster <helmut.schuster@sozialbau.at>
2019-09-30 08:31:26 +0200

    Test Datenbank-Migration

---
+++ src/main/resources/db/migration/V2__insert_products.sql
@@ -0,0 +1,4 @@
+
+insert into product(id, name, price) values (1, 'eins', 1.1);
+insert into product(id, name, price) values (2, 'zwei', 2.2);
+insert into product(id, name, price) values (3, 'drei', 3.3);
